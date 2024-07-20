fd7de2144c8f6a300c9fc9eb7ba2a7f351031f77
JeongMinE2 <130267954+JeongMinE2@users.noreply.github.com>
2024-07-20 15:51:20 +0900

    ✨ 로그인 텍스트 변경 기능 추가

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const btn = document.getElementById("loginBtn");
 const modal = document.getElementById("login-modal-background");
 const loginBtn = document.getElementById("login");
-const joinBtn = document.getElementById("join");
 
 btn.onclick = function() {
     modal.style.display = "block";
@@ -10,9 +9,5 @@ btn.onclick = function() {
 loginBtn.onclick = function() {
     modal.style.display = "none";
     alert("로그인은 안 해도 돼요~^^");
-}
-
-joinBtn.onclick = function() {
-    modal.style.display = "none";
-    alert("회원가입은 안 해도 돼요~^^");
+    btn.innerText = '로그아웃';
 }
