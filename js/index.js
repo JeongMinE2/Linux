85e762b92a9419b4910f5ddf540aaa161147315a
JeongMinE2 <130267954+JeongMinE2@users.noreply.github.com>
2024-07-28 22:00:15 +0900

    ✨ 로그아웃 버튼 클릭 시, 로그아웃 되고 알림창 기능 추가

--- js/index.js
+++ js/index.js
@@ -3,7 +3,12 @@ const modal = document.getElementById("login-modal-background");
 const loginBtn = document.getElementById("login");
 
 btn.onclick = function() {
-    modal.style.display = "block";
+    if (btn.innerText === '로그인') {
+        modal.style.display = "block";
+    } else {
+        btn.innerText = '로그인';
+        alert("로그아웃 되었습니다.");
+    }
 }
 
 loginBtn.onclick = function() {
